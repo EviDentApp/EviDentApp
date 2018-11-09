14a8955fa901eee561905cd67514727ce8d06e8f
Felipe Fronchetti <fronchetti@usp.br>
2018-11-09 11:24:56 -0200

    Slide layout done. Leads working. #16 Signed-off-by: Felipe Caetano Silva <felipecaetanosilva@hotmail.com> Signed-off-by: Felipe Fronchetti <fronchetti@usp.br>

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { GoogleAnalytics } from '@ionic-native/google-analytics/ngx';
 
 @Component({
   selector: 'app-intro',
@@ -8,15 +9,32 @@ import { Storage } from '@ionic/storage';
   styleUrls: ['./intro.page.scss'],
 })
 export class IntroPage implements OnInit {
+  public gender: any;
+  public age: any;
 
   constructor(private storage: Storage,
-              private router: Router) { }
+              private router: Router,
+              private ga: GoogleAnalytics) { }
 
   ngOnInit() {
   }
+
   async finish() {
-    await this.storage.set('tutorialComplete', true);
+    await this.storage.set('tutorialComplete', false);
+    await this.storage.set('gender', this.gender);
+    await this.storage.set('age', this.age);
+
+    this.ga.startTrackerWithId('UA-128523572-1')
+    .then(() => {
+      console.log('Google analytics is ready now');
+      this.ga.trackEvent("lead", "input", "age", this.age, false);
+      this.ga.trackEvent("gender", "input", this.gender, 1, false);
+    })
+    .catch(e => console.log('Error starting GoogleAnalytics', e));
+
     this.router.navigateByUrl('/');
   }
 
 }
+
+
