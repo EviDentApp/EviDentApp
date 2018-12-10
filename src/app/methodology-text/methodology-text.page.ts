e4cee0f20eb005d610d3dffbf329b7037cec9b7f
Thatiane Rosa <thatiane@gmail.com>
2018-12-10 17:02:44 -0200

    Remove icon from home page. Update intro slides text. Rename the relevance label in text-detail and methodology-text pages.
    Signed-off-by: Thatiane Rosa <thatiane@gmail.com>
    signed-off-by: Felipe Fronchetti <fronchetti@usp.br>
    Signed-off-by: Thatiane Rosa <thatiane@gmail.com>

--- src/app/methodology-text/methodology-text.page.ts
+++ src/app/methodology-text/methodology-text.page.ts
@@ -25,27 +25,27 @@ export class MethodologyTextPage implements OnInit {
   mapThermometer(value) {
     switch (value) {
       case '1': {
-        this.thermometer = 'O grau de relevância é muito baixo';
+        this.thermometer = 'O grau de evidência é muito baixo';
         this.thermometer_color = '#cedaed';
         break;
       }
       case '2': {
-        this.thermometer = 'O grau de relevância é baixo';
+        this.thermometer = 'O grau de evidência é baixo';
         this.thermometer_color = '#cee5ed';
         break;
       }
       case '3': {
-        this.thermometer = 'O grau de relevância é médio';
+        this.thermometer = 'O grau de evidência é médio';
         this.thermometer_color = '#ffffc6';
         break;
       }
       case '4': {
-        this.thermometer = 'O grau de relevância é alto';
+        this.thermometer = 'O grau de evidência é alto';
         this.thermometer_color = '#ffdcc6';
         break;
       }
       case '5': {
-        this.thermometer = 'O grau de relevância é muito alto';
+        this.thermometer = 'O grau de evidência é muito alto';
         this.thermometer_color = '#edcece';
         break;
       }
