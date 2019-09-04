6002820fc4ce9a9eea8aeb4569e76bb74595f576
Éderson Cássio <edersoncassio1983@gmail.com>
2019-09-03 22:30:32 -0300

    Primeiros testes - descobrindo como testar métodos assíncronos
    Co-authored-by: Vitor Ricardo <vitor.ricardo.oliveira@gmail.com>

--- src/app/guards/ebapp.guard.spec.ts
+++ src/app/guards/ebapp.guard.spec.ts
@@ -1,11 +1,44 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
 
 import { EbappGuard } from './ebapp.guard';
 
 describe('EbappGuard', () => {
+
+  let storage, router, guard;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [EbappGuard]
+    storage = jasmine.createSpyObj('Storage', {'get': false});
+    router = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    guard = new EbappGuard(storage, router);
+  });
+ 
+  it('should create the guard', () => {
+    expect(guard).toBeTruthy();
+  });
+
+  it('should query for tutorialComplete in Storage', () => {
+    guard.canActivate().then((value) => {
+      expect(value).toEqual(false);
+      expect(storage.get).toHaveBeenCalledTimes(1);
+      expect(storage.get).toHaveBeenCalledWith('tutorialComplete');
     });
   });
+
+  it('should navigate to /intro if tutorial is not complete', () => {
+    guard.canActivate().then((value) => {
+      expect(value).toEqual(false);
+      expect(router.navigateByUrl).toHaveBeenCalledTimes(1);
+      expect(router.navigateByUrl).toHaveBeenCalledWith('/intro');
+    });
+  });
+
+  it('should not navigate to /intro if tutorial is complete', () => {
+    storage = jasmine.createSpyObj('Storage', {'get': true});
+    guard = new EbappGuard(storage, router);
+    guard.canActivate().then((value) => {
+      expect(value).toEqual(true);
+      expect(router.navigateByUrl).toHaveBeenCalledTimes(0);
+    });
+  });
+
 });
