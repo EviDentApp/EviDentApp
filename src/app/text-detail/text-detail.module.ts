24eae7a12dbeff550b6f3b10573c54a4831a870b
rick_oliver <rick_oliver83@yahoo.com>
2019-10-01 16:22:50 -0300

    Ajustes para correcao de limitacao de editor de texto Co-authored-by: ederson cassio <edersoncassio1983@gmail.com>

--- src/app/text-detail/text-detail.module.ts
+++ src/app/text-detail/text-detail.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { TextDetailPage } from './text-detail.page';
+import { QuillModule } from 'ngx-quill';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    QuillModule.forRoot(),
     RouterModule.forChild(routes)
   ],
   declarations: [TextDetailPage]
