acc27b1e67c18061f73585fd0a64c95c86424ecd
Éderson Cássio <edersoncassio1983@gmail.com>
2019-10-04 04:26:10 -0300

    Tratamento do botão Voltar do Android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { GoogleAnalytics } from '@ionic-native/google-analytics/ngx';
 
+import { Router, NavigationEnd } from '@angular/router';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -17,14 +20,36 @@ export class AppComponent {
       icon: 'home'
     }
   ];
+  
+  private navLinksArray = [];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private ga: GoogleAnalytics
+    private ga: GoogleAnalytics,
+    private router: Router
   ) {
     this.initializeApp();
+    this.router.events.subscribe(event =>{
+      const url = this.router.url;
+      if (event instanceof NavigationEnd) {
+        const isCurrentUrlSaved = this.navLinksArray.find((item) => item === url);
+        if (!isCurrentUrlSaved) this.navLinksArray.push(url);
+      }
+    });
+    this.hardwareBackButton();
+  }
+
+  hardwareBackButton() {
+    this.platform.backButton.subscribe(() => {
+      if (this.navLinksArray.length > 1) {
+        this.navLinksArray.pop();
+        const index = this.navLinksArray.length - 1;
+        const url = this.navLinksArray[index];
+        this.router.navigate([url]);
+      }
+    });
   }
 
   initializeApp() {
