9524b78dd2993bbf791dcdf09cff634b6b281ea2
Éderson Cássio <edersoncassio1983@gmail.com>
2019-10-08 18:41:53 -0300

    Formulário de cadastro quase pronto Co-authored-by: vitor.ricardo.oliveira@gmail.com

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { RequisitionsService } from '../requisitions.service';
+import { UtilService } from '../util.service';
 
 
 @Component({
@@ -9,12 +12,98 @@ import { Storage } from '@ionic/storage';
 })
 export class RegisterPage implements OnInit {
 
-  constructor(private storage: Storage) { }
+  private facebook_id;
+  private google_id;
+  private name;
+  private email;
+  private email_password;
+  private birth_year;
+  private gender;
+  private grade_year;
+  private grade_state;
+
+  private show_password = true;
+
+  private states = [
+    { id: 'AC', name: 'Acre' },
+    { id: 'AL', name: 'Alagoas' },
+    { id: 'AP', name: 'Amapá' },
+    { id: 'AM', name: 'Amazonas' },
+    { id: 'BA', name: 'Bahia' },
+    { id: 'CE', name: 'Ceará' },
+    { id: 'DF', name: 'Distrito Federal' },
+    { id: 'ES', name: 'Espírito Santo' },
+    { id: 'GO', name: 'Goías' },
+    { id: 'MA', name: 'Maranhão' },
+    { id: 'MT', name: 'Mato Grosso' },
+    { id: 'MS', name: 'Mato Grosso do Sul' },
+    { id: 'MG', name: 'Minas Gerais' },
+    { id: 'PA', name: 'Pará' },
+    { id: 'PB', name: 'Paraíba' },
+    { id: 'PR', name: 'Paraná' },
+    { id: 'PE', name: 'Pernambuco' },
+    { id: 'PI', name: 'Piauí' },
+    { id: 'RJ', name: 'Rio de Janeiro' },
+    { id: 'RN', name: 'Rio Grande do Norte' },
+    { id: 'RS', name: 'Rio Grande do Sul' },
+    { id: 'RO', name: 'Rondônia' },
+    { id: 'RR', name: 'Roraíma' },
+    { id: 'SC', name: 'Santa Catarina' },
+    { id: 'SP', name: 'São Paulo' },
+    { id: 'SE', name: 'Sergipe' },
+    { id: 'TO', name: 'Tocantins' },
+  ];
+
+  constructor(
+    private storage: Storage,
+    private requisition: RequisitionsService,
+    private utilFunctions: UtilService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.storage.get('facebookData').then(user => {
-      alert('usuario -- ' + user);
+      if (user) {
+        user = JSON.parse(user);
+        this.name = user.name;
+        this.email = user.email;
+        this.facebook_id = user.id;
+        this.show_password = false;
+        this.storage.remove('facebookData');
+      }
     });
+  }
+
+  async register() {
+    let obj = {
+      "name": this.name,
+      "facebook_id": this.facebook_id,
+      "google_id": this.google_id,
+      "email": this.email,
+      "email_password": this.email_password,
+      "birth_year": this.birth_year,
+      "grade_year": this.grade_year,
+      "gender": this.gender,
+      "grade_state": this.grade_state,
+    }
+
+    alert(JSON.stringify(obj))
+
+    this.requisition.dentistCreate(obj).subscribe(
+      data => {
+        const response = (data as any);
+        const returned_object = JSON.parse(response._body);
+        if (returned_object._id) {
+          this.storage.set('user', JSON.stringify(returned_object));
+          this.storage.set('isLoggedIn', true);
+          this.router.navigateByUrl('/home');
+        }
+      },
+      error => {
+        console.log(error);
+        this.utilFunctions.presentAlert(error);
+      }
+    );
   }
 
 }
--- src/app/requisitions.service.ts
+++ src/app/requisitions.service.ts
@@ -8,7 +8,7 @@ import { UtilService } from './util.service';
 export class RequisitionsService {
   public key = this.utilFunctions.apiKey
   //public endpoint = "http://evident.tk" 
-  public endpoint = "http://192.168.42.148:5000" 
+  public endpoint = "http://192.168.0.103:5000" 
   public bodyThemesList = { "key": this.key }
   public bodyTextList = {
                           "key": this.key,
@@ -28,6 +28,19 @@ export class RequisitionsService {
 
   public bodyMethodologyArray = {"key": this.key,
                                  methodologies_list: []
+  }
+
+  public bodyDentistDetail = {
+    "key": this.key,
+    "name": "",
+    "facebook_id": null,
+    "google_id": null,
+    "email": "",
+    "email_password": "",
+    "birth_year": 0,
+    "grade_year": 0,
+    "gender": "",
+    "grade_state": "",
   }
 
   constructor(public http: Http,
@@ -55,6 +68,7 @@ export class RequisitionsService {
     this.bodyMethodologyDetail._id = metho_id;
     return this.http.post(this.endpoint + "/methodology/detail", this.bodyMethodologyDetail);
   }
+
   methodologiesGetArray(metho_array) {
     this.bodyMethodologyArray.methodologies_list = metho_array;
     return this.http.post(this.endpoint + "/methodologies/selected", this.bodyMethodologyArray);
@@ -69,4 +83,19 @@ export class RequisitionsService {
     var res = this.http.get(this.endpoint + "/dentists/?facebook_id="+ facebook_id)
     return res
   }
+
+  dentistCreate(dentist_obj) {
+    this.bodyDentistDetail.name = dentist_obj.name;
+    this.bodyDentistDetail.facebook_id = dentist_obj.facebook_id;
+    this.bodyDentistDetail.google_id = dentist_obj.google_id;
+    this.bodyDentistDetail.email = dentist_obj.email;
+    this.bodyDentistDetail.email_password = dentist_obj.email_password;
+    this.bodyDentistDetail.birth_year = dentist_obj.birth_year;
+    this.bodyDentistDetail.grade_year = dentist_obj.grade_year;
+    this.bodyDentistDetail.gender = dentist_obj.gender;
+    this.bodyDentistDetail.grade_state = dentist_obj.grade_state;
+    var res = this.http.post(this.endpoint + "/dentists/create", this.bodyDentistDetail);
+    return res
+  }
+
 }
